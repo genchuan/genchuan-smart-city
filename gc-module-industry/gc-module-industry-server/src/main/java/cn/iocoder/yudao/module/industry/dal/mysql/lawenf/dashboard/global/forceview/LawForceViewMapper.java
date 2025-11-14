67fc6896fe9a3e7424efd8ad34b0fd535b2e153a
liangxs <Alice72336E@qq.com>
2025-11-14 15:56:20 +0800

    [add]:综合执法大屏总览-执法力量分布视图

---
+++ gc-module-industry/gc-module-industry-server/src/main/java/cn/iocoder/yudao/module/industry/dal/mysql/lawenf/dashboard/global/forceview/LawForceViewMapper.java
@@ -0,0 +1,34 @@
+package cn.iocoder.yudao.module.industry.dal.mysql.lawenf.dashboard.global.forceview;
+
+
+import cn.iocoder.yudao.module.industry.controller.admin.lawenf.dashboard.global.forceview.vo.*;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 执法力量分布视图 Mapper
+ *
+ * @author lxs
+ */
+@Mapper
+public interface LawForceViewMapper {
+
+        /**
+         * 查询执法力量分布视图
+         *
+         * @param lawForceViewQueryReqVO 查询参数
+         * @return LawForceViewRespVO 结果
+         */
+        LawForceViewRespVO getLawForceView(LawForceViewQueryReqVO lawForceViewQueryReqVO);
+
+        List<LawForceStaffLocVO> selectStaffLoc(@Param("regionShortCode") String regionShortCode);
+
+        List<LawForceVehicleLocVO> selectVehicleLoc(@Param("regionShortCode") String regionShortCode);
+
+        List<LawForceStationLocVO> selectStationLoc(@Param("regionShortCode") String regionShortCode);
+
+        LawForceStaffCountVO selectRegionStaffCount(@Param("regionShortCode") String regionShortCode);
+
+}
---
+++ gc-module-industry/gc-module-industry-server/src/main/java/cn/iocoder/yudao/module/industry/service/lawenf/dashboard/global/forceview/LawForceViewServiceImpl.java
@@ -0,0 +1,33 @@
+package cn.iocoder.yudao.module.industry.service.lawenf.dashboard.global.forceview;
+
+
+import cn.iocoder.yudao.module.industry.controller.admin.lawenf.dashboard.global.forceview.vo.LawForceViewQueryReqVO;
+import cn.iocoder.yudao.module.industry.controller.admin.lawenf.dashboard.global.forceview.vo.LawForceViewRespVO;
+
+import cn.iocoder.yudao.module.industry.dal.mysql.lawenf.dashboard.global.forceview.LawForceViewMapper;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+/**
+ * 执法力量分布视图 Service 实现类
+ *
+ * @author lxs
+ */
+@Service
+@Validated
+public class LawForceViewServiceImpl implements LawForceViewService {
+
+    @Resource
+    private LawForceViewMapper lawForceViewMapper;
+
+    @Override
+    public LawForceViewRespVO getLawForceView(LawForceViewQueryReqVO lawForceViewQueryReqVO) {
+        LawForceViewRespVO respVO = new LawForceViewRespVO();
+        respVO.setStaffLocList(lawForceViewMapper.selectStaffLoc(lawForceViewQueryReqVO.getRegionShortCode()));
+        respVO.setVehicleLocList(lawForceViewMapper.selectVehicleLoc(lawForceViewQueryReqVO.getRegionShortCode()));
+        respVO.setStationLocList(lawForceViewMapper.selectStationLoc(lawForceViewQueryReqVO.getRegionShortCode()));
+        respVO.setRegionStaffCount(lawForceViewMapper.selectRegionStaffCount(lawForceViewQueryReqVO.getRegionShortCode()));
+        return respVO;
+    }
+}
