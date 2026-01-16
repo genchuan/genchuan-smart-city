92dc3569cdaaa406e36a1285ccdc194ffb1a584c
liangxs <Alice72336E@qq.com>
2026-01-16 15:48:25 +0800

    新增停车服务模块

---
+++ gc-module-park/gc-module-park-server/src/main/java/cn/iocoder/yudao/module/park/ParkApplication.java
@@ -0,0 +1,18 @@
+package cn.iocoder.yudao.module.park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目的启动类
+ *
+ * @author 芋道源码
+ */
+@SpringBootApplication
+public class ParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkApplication.class, args);
+    }
+
+}
