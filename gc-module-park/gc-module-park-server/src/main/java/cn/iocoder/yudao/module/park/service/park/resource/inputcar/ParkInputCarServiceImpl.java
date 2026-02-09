787f43c28e14f021dfdc019c93fe4d86a84dd42a
liangxusong <liangxusong@genchuan.cn>
2026-02-09 08:05:03 +0000

    Edit ParkInputCarServiceImpl.java

--- gc-module-park/gc-module-park-server/src/main/java/cn/iocoder/yudao/module/park/service/park/resource/inputcar/ParkInputCarServiceImpl.java
+++ gc-module-park/gc-module-park-server/src/main/java/cn/iocoder/yudao/module/park/service/park/resource/inputcar/ParkInputCarServiceImpl.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
