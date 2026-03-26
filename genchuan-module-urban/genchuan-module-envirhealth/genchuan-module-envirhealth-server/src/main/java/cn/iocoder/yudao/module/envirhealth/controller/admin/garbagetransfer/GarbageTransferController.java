1b4c2cf012599bf9d1eaad87bec33bd4a5e54eff
wzb0209 <16280882+wzb0209@user.noreply.gitee.com>
2026-03-26 17:35:38 +0800

    03-26 垃圾转运站模块 批量删除

--- genchuan-module-urban/genchuan-module-envirhealth/genchuan-module-envirhealth-server/src/main/java/cn/iocoder/yudao/module/envirhealth/controller/admin/garbagetransfer/GarbageTransferController.java
+++ genchuan-module-urban/genchuan-module-envirhealth/genchuan-module-envirhealth-server/src/main/java/cn/iocoder/yudao/module/envirhealth/controller/admin/garbagetransfer/GarbageTransferController.java
@@ -67,6 +67,15 @@ public class GarbageTransferController {
         return success(true);
     }
 
+    @DeleteMapping("/delete-batch")
+    @Operation(summary = "批量删除垃圾转运站")
+    @Parameter(name = "ids", description = "编号列表", required = true)
+    @PreAuthorize("@ss.hasPermission('envirhealth:garbage-transfer:delete')")
+    public CommonResult<Boolean> deleteGarbageTransferBatch(@RequestBody List<Long> ids) {
+        garbageTransferService.deleteGarbageTransferBatch(ids);
+        return success(true);
+    }
+
     @GetMapping("/get")
     @Operation(summary = "获得垃圾转运站")
     @Parameter(name = "id", description = "编号", required = true, example = "1024")
