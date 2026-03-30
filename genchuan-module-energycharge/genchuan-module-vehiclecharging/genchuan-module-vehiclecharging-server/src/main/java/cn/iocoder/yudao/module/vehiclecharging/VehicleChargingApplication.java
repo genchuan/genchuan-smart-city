cdb2fcbda6fc6e51c818224837bf1677fc7da745
zhucongquan <1197558781@qq.com>
2026-03-30 15:41:47 +0800

    03-30-能源充电-汽车充电模块初始化

---
+++ genchuan-module-energycharge/genchuan-module-vehiclecharging/genchuan-module-vehiclecharging-server/src/main/java/cn/iocoder/yudao/module/vehiclecharging/VehicleChargingApplication.java
@@ -0,0 +1,16 @@
+package cn.iocoder.yudao.module.vehiclecharging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class VehicleChargingApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleChargingApplication.class, args);
+    }
+}
