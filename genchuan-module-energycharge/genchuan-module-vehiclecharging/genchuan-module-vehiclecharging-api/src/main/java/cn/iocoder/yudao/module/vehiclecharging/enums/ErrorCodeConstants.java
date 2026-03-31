4de91bd0ba46618fab47ff33325f692ea3b383a1
zhucongquan <1197558781@qq.com>
2026-03-31 15:16:23 +0800

    03-31-充电车位模块开发

--- genchuan-module-energycharge/genchuan-module-vehiclecharging/genchuan-module-vehiclecharging-api/src/main/java/cn/iocoder/yudao/module/vehiclecharging/enums/ErrorCodeConstants.java
+++ genchuan-module-energycharge/genchuan-module-vehiclecharging/genchuan-module-vehiclecharging-api/src/main/java/cn/iocoder/yudao/module/vehiclecharging/enums/ErrorCodeConstants.java
@@ -12,5 +12,7 @@ public interface ErrorCodeConstants {
     // ========== 充电车位==========
     ErrorCode CHARGING_LOT_NOT_EXISTS = new ErrorCode(500, "充电车位不存在");
 
-
+    // ========== 互联互通表错误码=========
+    ErrorCode INTERCONNECTION_NOT_EXISTS = new ErrorCode(500, "互联互通表不存在");
+    ErrorCode INTERCONNECTION_STATUS_ERROR = new ErrorCode(500, "互联互通表状态错误");
 }
