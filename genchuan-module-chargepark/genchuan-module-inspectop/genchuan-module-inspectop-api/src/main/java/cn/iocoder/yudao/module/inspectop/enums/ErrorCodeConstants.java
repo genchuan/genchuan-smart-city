4ea2fac2224a3b9f5287309777dbe43f127eef08
zhucongquan <1197558781@qq.com>
2026-04-14 09:03:14 +0800

    04-14-充停一体巡查巡检初始化、gateway配置

---
+++ genchuan-module-chargepark/genchuan-module-inspectop/genchuan-module-inspectop-api/src/main/java/cn/iocoder/yudao/module/inspectop/enums/ErrorCodeConstants.java
@@ -0,0 +1,14 @@
+package cn.iocoder.yudao.module.inspectop.enums;
+
+
+import cn.iocoder.yudao.framework.common.exception.ErrorCode;
+
+/**
+ * inspectop 错误码枚举类
+ * inspectop 系统，使用 500 错误码
+ */
+public interface ErrorCodeConstants {
+
+
+
+}
