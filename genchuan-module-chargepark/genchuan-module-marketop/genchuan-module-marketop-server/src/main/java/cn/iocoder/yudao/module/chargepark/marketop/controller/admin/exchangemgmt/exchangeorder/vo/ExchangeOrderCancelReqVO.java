f955d03a0d3ab434ab9683143fdb2d2cc6dd6277
qiye <stazyt@163.com>
2026-04-23 15:48:29 +0800

    兑换订单修改

---
+++ genchuan-module-chargepark/genchuan-module-marketop/genchuan-module-marketop-server/src/main/java/cn/iocoder/yudao/module/chargepark/marketop/controller/admin/exchangemgmt/exchangeorder/vo/ExchangeOrderCancelReqVO.java
@@ -0,0 +1,15 @@
+package cn.iocoder.yudao.module.chargepark.marketop.controller.admin.exchangemgmt.exchangeorder.vo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Schema(description = "管理后台 - 兑换订单取消 Request VO")
+@Data
+public class ExchangeOrderCancelReqVO {
+
+    @Schema(description = "兑换订单ID", requiredMode = Schema.RequiredMode.REQUIRED)
+    @NotNull(message = "兑换订单ID不能为空")
+    private Long id;
+
+}
