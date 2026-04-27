4914b874cc96ca7db197587e504e6d28a6a1b4b9
qiye <stazyt@163.com>
2026-04-27 17:04:47 +0800

    fix：优惠券重新发放

--- genchuan-module-chargepark/genchuan-module-marketop/genchuan-module-marketop-server/src/main/java/cn/iocoder/yudao/module/chargepark/marketop/service/couponactivity/couponmgmt/CouponMgmtServiceImpl.java
+++ genchuan-module-chargepark/genchuan-module-marketop/genchuan-module-marketop-server/src/main/java/cn/iocoder/yudao/module/chargepark/marketop/service/couponactivity/couponmgmt/CouponMgmtServiceImpl.java
@@ -11,6 +11,7 @@ import cn.iocoder.yudao.module.chargepark.marketop.controller.admin.couponactivi
 import cn.iocoder.yudao.module.chargepark.marketop.controller.admin.couponactivity.couponmgmt.vo.CouponMgmtUpdateReqVO;
 import cn.iocoder.yudao.module.chargepark.marketop.dal.dataobject.couponactivity.CouponMgmtDO;
 import cn.iocoder.yudao.module.chargepark.marketop.dal.mysql.couponactivity.CouponMgmtMapper;
+import cn.iocoder.yudao.module.chargepark.marketop.enums.CouponMgmtStatusEnum;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -69,7 +70,7 @@ public class CouponMgmtServiceImpl implements CouponMgmtService {
 //        if (!"0".equals(couponMgmt.getStatus())) {
 //            throw exception(COUPON_MGMT_STATUS_ERROR);
 //        }
-        couponMgmt.setStatus("1");
+        couponMgmt.setStatus(CouponMgmtStatusEnum.RECEIVED.getValue());
         Long loginUserId = SecurityFrameworkUtils.getLoginUserId();
         couponMgmt.setSenderId(loginUserId); // 发放人为当前操作用户，由Controller层设置
         couponMgmt.setSendTime(LocalDateTime.now());
@@ -83,7 +84,7 @@ public class CouponMgmtServiceImpl implements CouponMgmtService {
 //        if (!"1".equals(couponMgmt.getStatus())) {
 //            throw exception(COUPON_MGMT_STATUS_ERROR);
 //        }
-        couponMgmt.setStatus("2");
+        couponMgmt.setStatus(CouponMgmtStatusEnum.USED.getValue());
         couponMgmt.setVerifyTime(LocalDateTime.now());
         couponMgmtMapper.updateById(couponMgmt);
     }
@@ -95,7 +96,7 @@ public class CouponMgmtServiceImpl implements CouponMgmtService {
 //            throw exception(COUPON_MGMT_STATUS_ERROR);
 //        }
         Long loginUserId = SecurityFrameworkUtils.getLoginUserId();
-        couponMgmt.setStatus("1");
+        couponMgmt.setStatus(CouponMgmtStatusEnum.NOT_RECEIVED.getValue());
         couponMgmt.setSenderId(loginUserId);
         couponMgmt.setSendTime(LocalDateTime.now());
         couponMgmt.setReceiverId(receiverId);
