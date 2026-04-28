4304db74d9aad46a6493a1f30c4a10df20e8e7da
sujiale <2756485492@qq.com>
2026-04-28 11:50:42 +0800

    修复充电桩编号筛选功能/告警时间筛选功能

--- genchuan-module-energycharge/genchuan-module-vehiclecharging/genchuan-module-vehiclecharging-server/src/main/java/cn/iocoder/yudao/module/vehiclecharging/controller/admin/pilealarm/vo/NewPileAlarmPageReqVO.java
+++ genchuan-module-energycharge/genchuan-module-vehiclecharging/genchuan-module-vehiclecharging-server/src/main/java/cn/iocoder/yudao/module/vehiclecharging/controller/admin/pilealarm/vo/NewPileAlarmPageReqVO.java
@@ -3,6 +3,7 @@ package cn.iocoder.yudao.module.vehiclecharging.controller.admin.pilealarm.vo;
 import cn.iocoder.yudao.framework.common.pojo.PageParam;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -29,8 +30,10 @@ public class NewPileAlarmPageReqVO extends PageParam {
     private String alarmStatus;
 
     @Schema(description = "告警开始时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime alarmTimeStart;
 
     @Schema(description = "告警结束时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime alarmTimeEnd;
 }
