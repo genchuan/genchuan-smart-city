bd2bd2d31f2fdcf79fc362a7bd9aa5fdfd8a5168
高绍龙 <gsllong@126.com>
2026-05-06 14:38:58 +0800

    改站场名

--- genchuan-module-chargepark/genchuan-module-ordertrade/genchuan-module-ordertrade-server/src/main/java/cn/iocoder/yudao/module/ordertrade/controller/admin/ordermgmt/vo/ShareChargeOrderPageReqVO.java
+++ genchuan-module-chargepark/genchuan-module-ordertrade/genchuan-module-ordertrade-server/src/main/java/cn/iocoder/yudao/module/ordertrade/controller/admin/ordermgmt/vo/ShareChargeOrderPageReqVO.java
@@ -30,8 +30,8 @@ public class ShareChargeOrderPageReqVO extends PageParam {
     @ExcelProperty(value = "订单状态", converter = EnumExcelConverter.class)
     @EnumFormat(ChargeOrderStatusEnum.class)   // ← 指向枚举类
     private String status;
-    @Schema(description = "场站ID")
-    private Long stationId;
+    @Schema(description = "场站名称，模糊查询")
+    private String stationName;
     @Schema(description = "借出时间-开始，格式 yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
     private LocalDateTime lendTimeStart;
