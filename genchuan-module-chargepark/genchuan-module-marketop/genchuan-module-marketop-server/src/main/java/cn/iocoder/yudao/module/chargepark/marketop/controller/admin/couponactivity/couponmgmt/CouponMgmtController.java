ecfbcafb85181fb4b7e1f4183e61349d5732522a
qiye <stazyt@163.com>
2026-05-13 15:05:01 +0800

    feat: 新增优惠券下拉列表

--- genchuan-module-chargepark/genchuan-module-marketop/genchuan-module-marketop-server/src/main/java/cn/iocoder/yudao/module/chargepark/marketop/controller/admin/couponactivity/couponmgmt/CouponMgmtController.java
+++ genchuan-module-chargepark/genchuan-module-marketop/genchuan-module-marketop-server/src/main/java/cn/iocoder/yudao/module/chargepark/marketop/controller/admin/couponactivity/couponmgmt/CouponMgmtController.java
@@ -8,6 +8,7 @@ import cn.iocoder.yudao.framework.common.util.object.BeanUtils;
 import cn.iocoder.yudao.framework.excel.core.util.ExcelUtils;
 import cn.iocoder.yudao.module.chargepark.marketop.controller.admin.couponactivity.couponmgmt.vo.*;
 import cn.iocoder.yudao.module.chargepark.marketop.dal.dataobject.couponactivity.CouponMgmtDO;
+import cn.iocoder.yudao.module.chargepark.marketop.enums.CouponMgmtStatusEnum;
 import cn.iocoder.yudao.module.chargepark.marketop.service.couponactivity.couponmgmt.CouponMgmtService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -145,6 +146,13 @@ public class CouponMgmtController {
         PageResult<CouponMgmtDO> pageResult = couponMgmtService.getPage(reqVO);
         List<CouponMgmtRespVO> list = BeanUtils.toBean(pageResult.getList(), CouponMgmtRespVO.class);
         ExcelUtils.write(response, "优惠券.xlsx", "数据", CouponMgmtRespVO.class, list);
+    }
+
+    @GetMapping("/simple-list")
+    @Operation(summary = "获取优惠券精简列表")
+    public CommonResult<List<CouponMgmtSimpleRespVO>> getSimpleList() {
+        List<CouponMgmtDO> list = couponMgmtService.getSimpleList();
+        return CommonResult.success(BeanUtils.toBean(list, CouponMgmtSimpleRespVO.class));
     }
 
     @GetMapping("/chart")
