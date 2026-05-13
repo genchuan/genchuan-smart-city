3b7743bd5d90b5fa0a6a20fc08fa2ecfb34c7e62
sucl <1@11.cc>
2026-05-13 17:10:06 +0800

    时间

--- genchuan-module-education-teaching/genchuan-module-studentmgmt/genchuan-module-studentmgmt-server/src/main/java/cn/iocoder/yudao/module/studentmgmt/controller/admin/mentalmgmt/vo/MentalMgmtConsultReqVO.java
+++ genchuan-module-education-teaching/genchuan-module-studentmgmt/genchuan-module-studentmgmt-server/src/main/java/cn/iocoder/yudao/module/studentmgmt/controller/admin/mentalmgmt/vo/MentalMgmtConsultReqVO.java
@@ -3,8 +3,11 @@ package cn.iocoder.yudao.module.studentmgmt.controller.admin.mentalmgmt.vo;
 import cn.idev.excel.annotation.ExcelProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
+
+import static cn.iocoder.yudao.framework.common.util.date.DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND;
 
 @Schema(description = "管理后台 - 心理管理预约 Request VO")
 @Data
@@ -15,6 +18,7 @@ public class MentalMgmtConsultReqVO {
     private Long id;
 
     @Schema(description = "咨询预约时间")
+    @DateTimeFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
     private LocalDateTime consultTime;
 
 }
