79d8621fce81c18b44a062070c26d7c39332146f
sucl <1@11.cc>
2026-05-14 15:46:16 +0800

    身份证验证

--- yudao-framework/yudao-common/src/main/java/cn/iocoder/yudao/framework/common/util/validation/ValidationUtils.java
+++ yudao-framework/yudao-common/src/main/java/cn/iocoder/yudao/framework/common/util/validation/ValidationUtils.java
@@ -24,6 +24,22 @@ public class ValidationUtils {
 
     private static final Pattern PATTERN_XML_NCNAME = Pattern.compile("[a-zA-Z_][\\-_.0-9_a-zA-Z$]*");
 
+    private static final Pattern PATTERN_ID_CARD = Pattern.compile("^\\d{6}(?:18|19|20)\\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12][0-9]|3[01])\\d{3}(\\d|X|x)$");
+
+    private static final int[] WEIGHTS = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
+    private static final char[] CHECK_CODES = {'1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'};
+
+    public static boolean isIdCard(String id) {
+        return StringUtils.hasText(id)
+                && PATTERN_ID_CARD.matcher(id).matches();
+
+//        int sum = 0;
+//        for (int i = 0; i < 17; i++) {
+//            sum += (id.charAt(i) - '0') * WEIGHTS[i];
+//        }
+//        int index = sum % 11;
+//        return CHECK_CODES[index] == id.charAt(17);
+    }
     public static boolean isMobile(String mobile) {
         return StringUtils.hasText(mobile)
                 && PATTERN_MOBILE.matcher(mobile).matches();
