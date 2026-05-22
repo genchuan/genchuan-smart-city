d7ad942726bd356de2b2813a9dd534bdcf21470a
sucl <1@11.cc>
2026-05-22 16:34:49 +0800

    文件上传端口号

--- genchuan-module-education-teaching/genchuan-module-studentmgmt/genchuan-module-studentmgmt-server/src/main/java/cn/iocoder/yudao/module/studentmgmt/framework/file/config/MinioConfig.java
+++ genchuan-module-education-teaching/genchuan-module-studentmgmt/genchuan-module-studentmgmt-server/src/main/java/cn/iocoder/yudao/module/studentmgmt/framework/file/config/MinioConfig.java
@@ -25,7 +25,7 @@ public class MinioConfig {
     public static class MinioProperties {
 
         private String endpoint = "http://192.168.8.68:9000";
-        private String publicEndpoint = "http://112.47.127.21:59000";
+        private String publicEndpoint = "http://112.47.127.21:9000";
         private String accessKey = "minioadmin";
         private String accessSecret = "minioadmin123";
         private String bucket = "genchuan";
