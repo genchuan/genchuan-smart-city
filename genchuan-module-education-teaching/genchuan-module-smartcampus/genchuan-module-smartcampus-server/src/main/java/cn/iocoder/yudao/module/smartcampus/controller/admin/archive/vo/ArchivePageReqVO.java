7203155d43782a006676350b105bd908bbc71cf7
wuwuhe <2587562433@qq.com>
2026-05-28 13:30:28 +0800

    完成智慧校园-学籍档案管理-基础功能、审批和维护接口

--- genchuan-module-education-teaching/genchuan-module-smartcampus/genchuan-module-smartcampus-server/src/main/java/cn/iocoder/yudao/module/smartcampus/controller/admin/archive/vo/ArchivePageReqVO.java
+++ genchuan-module-education-teaching/genchuan-module-smartcampus/genchuan-module-smartcampus-server/src/main/java/cn/iocoder/yudao/module/smartcampus/controller/admin/archive/vo/ArchivePageReqVO.java
@@ -1,7 +1,6 @@
 package cn.iocoder.yudao.module.smartcampus.controller.admin.archive.vo;
 
 import lombok.*;
-import java.util.*;
 import io.swagger.v3.oas.annotations.media.Schema;
 import cn.iocoder.yudao.framework.common.pojo.PageParam;
 import org.springframework.format.annotation.DateTimeFormat;
