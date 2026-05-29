b9e52fa454b4883368360ce795383b0bde6027ed
sucl <1@11.cc>
2026-05-29 16:08:23 +0800

    修复几个bug

--- genchuan-module-education-teaching/genchuan-module-studentmgmt/genchuan-module-studentmgmt-server/src/main/java/cn/iocoder/yudao/module/studentmgmt/service/accessapply/AccessApplyServiceImpl.java
+++ genchuan-module-education-teaching/genchuan-module-studentmgmt/genchuan-module-studentmgmt-server/src/main/java/cn/iocoder/yudao/module/studentmgmt/service/accessapply/AccessApplyServiceImpl.java
@@ -136,12 +136,16 @@ public class AccessApplyServiceImpl implements AccessApplyService {
         Long classId = reqVO.getClassId();
 
         // 通过classId 查询 System模块的 dept.id 的名称，减少关联查询
-        CommonResult<DeptRespDTO> dept = deptApi.getDept(classId);
-        DeptRespDTO data = dept.getData();
-        if (data == null) {
-            throw new ServiceException(ErrorCodeConstants.DEPT_NOT_EXISTS);
+        String className=null;
+        if (null == classId) {
+            CommonResult<DeptRespDTO> dept = deptApi.getDept(classId);
+            DeptRespDTO data = dept.getData();
+            if (data == null) {
+                throw new ServiceException(ErrorCodeConstants.DEPT_NOT_EXISTS);
+            }
+             className = data.getName();
+
         }
-        String className = data.getName();
 
         // 1. 卡片数据
         //totalCount (integer): 本期考评总记录数。
